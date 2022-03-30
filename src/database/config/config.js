de3c1b13bec55c332c68095fae3a3cf0569d9724
IsaacMagno <isaacmagno98@gmail.com>
2022-03-30 16:50:55 -0300

    heroku-fixed

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV;
+// const environment = process.env.NODE_ENV;
 
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
+// const suffix = {
+//   prod: "",
+//   production: "",
+//   dev: "-dev",
+//   development: "-dev",
+//   test: "-test",
+// };
 
 const options = {
   host: process.env.MYSQL_HOST || 'mysql',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'tobias-project'}${suffix[environment] || suffix.dev}`,
+    `${process.env.MYSQL_DB_NAME || 'tobias-project'}`,
   username: process.env.MYSQL_USER || 'b24ec32657ac79',
   password: process.env.MYSQL_PASSWORD || 'df457fc5',
   dialect: 'mysql',
