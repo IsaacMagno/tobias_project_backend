ba75dfb74215792be1fbefe3ef82849c530b4b3d
IsaacMagno <isaacmagno98@gmail.com>
2022-04-07 18:39:16 -0300

    adiciona scrap frases

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,15 +4,18 @@ const statisticsController = require("../controllers/statisticsController");
 const activitiesController = require("../controllers/activitiesController");
 const filesController = require("../controllers/filesController");
 const uploadImage = require("../middlewares/uploadImage");
+const phrasesController = require("../controllers/phrasesController");
 
 router.get("/", championsController.getAll);
 
-router.put("/stats/:id", statisticsController.updateStatistics);
-
-router.put("/activities/:id", activitiesController.updateActivities);
+router.get("/phrases", phrasesController.getPhrases);
 
 router.get("/uploads", filesController.getAllFiles);
 
 router.post("/uploads", uploadImage.single("file"), filesController.uploadFile);
+
+router.put("/stats/:id", statisticsController.updateStatistics);
+
+router.put("/activities/:id", activitiesController.updateActivities);
 
 module.exports = router;
